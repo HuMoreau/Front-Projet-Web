d095aaa6826e645a38fc3fe1a35b339727ade976
Delsus78 <delsustheugm@gmail.com>
2022-07-05 14:56:38 +0200

    OMG GG lES NOISETTES A ZERO EST PAS NULL INCROYABLE

--- src/components/HeadBar/UserHeadbarInfo.vue
+++ src/components/HeadBar/UserHeadbarInfo.vue
@@ -7,7 +7,7 @@
       <label id="nomUtilisateur" class="mb-0">{{userPrenom ? userPrenom : '-'}}
         {{userNom ? userNom : '-'}}</label>
       <div v-if="isUserDev" class="flex flex-row justify-content-center align-items-center">
-        <label id="pointsNoisette" class="mr-1">{{userNoisettes ? userNoisettes : '-'}}</label>
+        <label id="pointsNoisette" class="mr-1">{{userNoisettes}}</label>
         <img alt="logo" src="./../../assets/noisette.png" height="15">
       </div>
     </div>
