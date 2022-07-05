7625233869c49d0d5338573f757f30b162c97276
Delsus78 <delsustheugm@gmail.com>
2022-07-05 15:49:29 +0200

    OH AIME J AI

--- src/components/HomePage/HomePage.vue
+++ src/components/HomePage/HomePage.vue
@@ -1,32 +1,28 @@
 <template>
-  <div id="content" class="grid">
-    <div class="col-6 grid align-content-start">
-      <HelloDisplayer id="hello_displayer" class="col-12"></HelloDisplayer>
-      <SelectedTicketDisplayer id="selected_ticket_displayer" class="col-12"/>
-      <FreeTicketsDisplayer id="free_tickets_displayer" class="col-12"/>
-    </div>
-    <div class="col-6 grid align-content-start">
-      <InfoDevDisplayer class="col-12"></InfoDevDisplayer>
-      <ProjectsStateDisplayer id="etat_projet_displayer" class="col-12"/>
-    </div>
+  <div>
+    <HomePageDeveloppeur v-if="isUserDev"/>
+    <HomePageRapporteur v-if="isUserRap"/>
   </div>
 </template>
 
 <script>
-import FreeTicketsDisplayer from "@/components/HomePage/Displayers/FreeTicketsDisplayer";
-import InfoDevDisplayer from "@/components/HomePage/Displayers/InfoDevDisplayer";
-import ProjectsStateDisplayer from "@/components/HomePage/Displayers/ProjectsStateDisplayer";
-import SelectedTicketDisplayer from "@/components/HomePage/Displayers/SelectedTicketDisplayer";
-import HelloDisplayer from "@/components/HomePage/Displayers/HelloDisplayer";
+import HomePageDeveloppeur from "@/components/HomePage/HomePageDeveloppeur";
+import HomePageRapporteur from "@/components/HomePage/HomePageRapporteur";
+import {useAuthStore} from "@/store/authStore";
 
 export default {
   name: "HomePage",
   components : {
-    HelloDisplayer,
-    FreeTicketsDisplayer,
-    InfoDevDisplayer,
-    ProjectsStateDisplayer,
-    SelectedTicketDisplayer,
+    HomePageRapporteur,
+    HomePageDeveloppeur
+  },
+  setup() {
+    const authStore = useAuthStore();
+    const {isUserDev, isUserRap} = authStore;
+    return {
+      isUserDev,
+      isUserRap
+    }
   }
 }
 </script>
---
+++ src/components/HomePage/HomePageRapporteur.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="content" class="grid">
+    <div class="col-6 grid align-content-start">
+      <HelloDisplayer id="hello_displayer" class="col-12"></HelloDisplayer>
+      <SelectedTicketDisplayer id="selected_ticket_displayer" class="col-12"/>
+      <FreeTicketsDisplayer id="free_tickets_displayer" class="col-12"/>
+    </div>
+    <div class="col-6 grid align-content-start">
+      <InfoDevDisplayer class="col-12"></InfoDevDisplayer>
+      <ProjectsStateDisplayer id="etat_projet_displayer" class="col-12"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import FreeTicketsDisplayer from "@/components/HomePage/Displayers/FreeTicketsDisplayer";
+import InfoDevDisplayer from "@/components/HomePage/Displayers/InfoDevDisplayer";
+import ProjectsStateDisplayer from "@/components/HomePage/Displayers/ProjectsStateDisplayer";
+import SelectedTicketDisplayer from "@/components/HomePage/Displayers/SelectedTicketDisplayer";
+import HelloDisplayer from "@/components/HomePage/Displayers/HelloDisplayer";
+
+export default {
+  name: "HomePageRapporteur",
+  components : {
+    HelloDisplayer,
+    FreeTicketsDisplayer,
+    InfoDevDisplayer,
+    ProjectsStateDisplayer,
+    SelectedTicketDisplayer,
+  }
+}
+</script>
+
+<style scoped>
+</style>
