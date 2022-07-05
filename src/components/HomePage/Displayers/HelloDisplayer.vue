45e958d7dbd320a7e4ac38736f9ef3a2ac5e8dc0
Hugo MOREAU [8ff749c8-0e41-4edd-a491-4067a3d81970] <hugo.moreau@ca-ps.com>
2022-07-05 15:49:03 +0200

    Features Diverses

--- src/components/HomePage/Displayers/HelloDisplayer.vue
+++ src/components/HomePage/Displayers/HelloDisplayer.vue
@@ -1,16 +1,28 @@
 <template>
   <div class="p-component p-panel">
-    <div class="p-panel-header">
-      <label>BONJOUR</label>
+    <div class="p-panel-header quote-container flex flex-column">
+      <div class="mb-2 bonjour fadein">Bonjour <strong class="fadein2">{{this.userPrenom}} {{this.userNom}}</strong></div>
+      <div class="fadein3"><i>{{this.quote}}</i></div>
     </div>
   </div>
 </template>
 
 <script>
+import {useAuthStore} from "@/store/authStore";
+import {storeToRefs} from "pinia/dist/pinia.esm-browser";
+
 import {quoteService} from "@/main";
 
 export default {
   name: "HelloDisplayer",
+  setup() {
+    const authStore = useAuthStore();
+    const {userNom, userPrenom} = storeToRefs(authStore);
+    return{
+      userNom,
+      userPrenom,
+    }
+  },
   data(){
     return {
       quote : ""
@@ -31,5 +43,29 @@ export default {
 </script>
 
 <style scoped>
+@keyframes fadeIn {
+  0% { opacity: 0; }
+  100% { opacity: 1; }
+}
 
+.quote-container{
+  border-radius: 6px;
+  text-align: center;
+}
+
+.bonjour{
+  font-size: 1.5rem;
+}
+
+.fadein{
+  animation: fadeIn 2s;
+}
+
+.fadein2{
+  animation: fadeIn 4s;
+}
+
+.fadein3{
+  animation: fadeIn 6s;
+}
 </style>
