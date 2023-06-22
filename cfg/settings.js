aba0656e136feee5a630fcfc2058c1e242cb3cbf
Anthony Paboeuf <62709270+Delsus78@users.noreply.github.com>
2023-06-22 08:45:08 +0200

    Create settings.js

---
+++ cfg/settings.js
@@ -0,0 +1,9 @@
+(function(window) {
+  window.__env = window.__env || {};
+
+  window.__env.api = {
+    "url": "http://localhost:9090/api/",
+    "timeout": 5000
+  };
+  window.__env.debug = true;
+})(this);
